70dc6a6cc46e971a83f7c08ca364e8691767bca5
Sharangill00 <gills7812@gmail.com>
2022-06-22 18:13:57 +0200

    Initial commit

---
+++ src/test/java/at/technikum/datee/DateeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.datee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
